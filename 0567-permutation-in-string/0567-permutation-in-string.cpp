79e00500e9a969fa3373e5d731d5c1d0e1c197ee
Bhavyaa-jain <20jain.bhavya@gmail.com>
2026-08-18 12:35:15 +0530

    Time: 3 ms (65.83%), Space: 9.8 MB (89.6%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-
-    bool isFreqSame(int freq1[], int freq2[]){
-        for(int i=0; i<26; i++){
-            if(freq1[i] != freq2[i]){
-                return false;
-            }
-        }
-        return true;
-    }
     bool checkInclusion(string s1, string s2) {
-        int freq[26] = {0};
-        for(int i = 0; i<s1.length();i++){
-            freq[s1[i] -'a']++;
-        }
-        int windSize = s1.length();
+        
+        if(s1.size() > s2.size())
+            return false;
 
-        for(int i=0; i<s2.length(); i++){
-            int windIdx = 0, idx = i;
-            int windFreq[26] = {0};
-             while(windIdx < windSize && idx < s2.length()){
-                windFreq[s2[idx] - 'a']++;
-                windIdx++; idx++;
-             }
-             if(isFreqSame(freq, windFreq)){
+        int a[26] = {0};
+        int b[26] = {0};
+
+        for(char c : s1)
+            a[c - 'a']++;
+
+        for(int i = 0; i < s1.size(); i++)
+            b[s2[i] - 'a']++;
+
+        for(int i = s1.size(); i < s2.size(); i++) {
+
+            if(equal(a, a + 26, b))
                 return true;
-             }
+
+            b[s2[i] - 'a']++;
+            b[s2[i - s1.size()] - 'a']--;
         }
-        return false;
+
+        return equal(a, a + 26, b);
     }
 };
